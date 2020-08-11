c99c7a77572852fe56476a8e82ebb620652877f7
David Denton <daviddenton@users.noreply.github.com>
2020-08-11 11:04:49 +0000

    http4k 0.11.0

--- Formula/http4k.rb
+++ Formula/http4k.rb
@@ -1,8 +1,8 @@
 class Http4k < Formula
   desc "Functional HTTP toolkit implemented in pure Kotlin"
   homepage "https://http4k.org"
-  url "https://github.com/http4k/toolbox-cli/releases/download/v0.9.0/http4k-toolbox-0.9.0.zip"
-  sha256 "80fa86c5d419acb2f6bf623f75f355d9f31dcd0bb78a7eaa6365a33577cb7662"
+  url "https://github.com/http4k/toolbox-cli/releases/download/v0.11.0/http4k-toolbox-0.11.0.zip"
+  sha256 "43f7279311387b3760acde6fd8909a3da7cf23890e8cf658561b7d9f25ddf081"
 
   bottle :unneeded
 
