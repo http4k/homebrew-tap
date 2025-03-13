342383696f980db2a48aa6bedc118d523241cf81
David Denton <denton.david@gmail.com>
2025-03-13 08:51:51 +0000

    Change SHAs

--- Formula/http4k-mcp-desktop.rb
+++ Formula/http4k-mcp-desktop.rb
@@ -1,7 +1,7 @@
 class Http4kMcpDesktop < Formula
   desc "http4k MCP Desktop - multi-protocol MCP client application"
   homepage "https://http4k.org"
-  version "1.0.0"
+  version "0.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
