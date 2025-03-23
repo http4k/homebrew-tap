ebf291d8cc737045dd08091580c1dda0e1a62b55
David Denton <daviddenton@users.noreply.github.com>
2025-03-23 20:29:21 +0000

    http4k-mcp-desktop 1.1.0

--- Formula/http4k-mcp-desktop.rb
+++ Formula/http4k-mcp-desktop.rb
@@ -3,8 +3,8 @@ class Http4kMcpDesktop < Formula
   homepage "https://mcp.http4k.org"
 
   on_macos do
-      url "https://github.com/http4k/mcp-desktop/releases/download/1.0.0/http4k-mcp-desktop.macos-arm64"
-      sha256 "c685128c6e1eff33e0d228f6726c7d90744ae321fe89ae3675b5c1397e1f3e42"
+      url "https://github.com/http4k/mcp-desktop/releases/download/1.1.0/http4k-mcp-desktop.macos-arm64"
+      sha256 "924b9d83e17ebf3c700c0c7f712b2466556a2c1a3658586173c77aed63682ce3"
   end
 
   def install
